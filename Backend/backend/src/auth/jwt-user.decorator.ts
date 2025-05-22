3281a8f3fabe38bfc13e1952a930d64857af5a6b
cosdealpha <bocangelcamila@gmail.com>
2025-05-22 14:23:56 -0400

    backend funcional

--- Backend/backend/src/auth/jwt-user.decorator.ts
+++ Backend/backend/src/auth/jwt-user.decorator.ts
@@ -1,9 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    if (!request.user) {
+      throw new UnauthorizedException('User not found in request');
+    }
     return request.user;
   },
 );
